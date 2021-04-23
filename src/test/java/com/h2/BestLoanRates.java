f634a32f5b3026b664c59a2bfd1b4b29e31904b9
brayan Alexsander Caldeira de Oliveira <brayancaldeirabr@gmail.com>
2021-04-23 15:00:28 -0300

    Teste Inicial

--- src/test/java/com/h2/BestLoanRates.java
+++ src/test/java/com/h2/BestLoanRates.java
@@ -1,5 +1,6 @@
 package com.h2;
 
+
 import java.util.Map;
 import java.util.Scanner;
 
@@ -10,26 +11,28 @@ public class BestLoanRates {
             3, 2.67f
     );
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = scanner.nextLine();
-        System.out.println("Hello " + name);
+        System.out.println( "Hello " +  name  );
 
-        System.out.println("Enter the loan term in years)");
-        int loanTermInYears  = scanner.nextInt();
+        System.out.println("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt();
         float bestRate = getRates(loanTermInYears);
-        if(bestRate == 0.0f) {
-            System.out.println("No available rates for terms: " + loanTermInYears + " years");
-        } else {
-            System.out.println("Best available rate: " + getRates(loanTermInYears) + "%");
+        if(bestRate == 0.0f ) {
+            System.out.println( "No available rates for term: " + loanTermInYears + " years");
+        }else {
+            System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
         }
+        scanner.close();
     }
-
-    public static float getRates (int loanTermInYears) {
-        if(bestRates.containsKey(loanTermInYears)) {
+    public static float getRates(int loanTermInYears){
+        if(bestRates.containsKey(loanTermInYears)){
             return bestRates.get(loanTermInYears);
+
         }
         return 0.0f;
     }
+
 }
